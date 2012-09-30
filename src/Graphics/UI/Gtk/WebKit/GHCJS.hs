452231c0df5d2a42c87e27fc9d274775ed967e02
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-10-01 03:31:28 +1300

    Improve runWebGUI

--- src/Graphics/UI/Gtk/WebKit/GHCJS.hs
+++ src/Graphics/UI/Gtk/WebKit/GHCJS.hs
@@ -8,12 +8,25 @@ module Graphics.UI.Gtk.WebKit.GHCJS (
 import System.Glib.FFI
 import Graphics.UI.Gtk.WebKit.Types
 import Foreign (ForeignPtr, nullPtr, Ptr)
-import Control.Monad (liftM)
-import Graphics.UI.Gtk.WebKit.WebView (webViewNew)
+import Control.Monad (unless, forever, liftM)
+import Graphics.UI.Gtk.WebKit.WebView
+       (webViewSetWebSettings, webViewGetWebSettings, loadStarted,
+        webViewLoadUri, loadFinished, webViewNew)
 import Graphics.UI.Gtk
-       (widgetShowAll, mainQuit, onDestroy, WindowPosition(..),
-        containerAdd, scrolledWindowNew, windowSetPosition,
-        windowSetDefaultSize, windowNew, mainGUI, initGUI)
+       (timeoutAddFull, widgetShowAll, mainQuit, onDestroy,
+        WindowPosition(..), containerAdd, scrolledWindowNew,
+        windowSetPosition, windowSetDefaultSize, windowNew, mainGUI,
+        initGUI)
+import Control.Concurrent
+       (yield, threadDelay, takeMVar, newEmptyMVar)
+import System.Glib.Signals (on)
+import System.Environment (getArgs)
+import System.Glib.Attributes (get, AttrOp(..), set)
+import Graphics.UI.Gtk.WebKit.WebSettings (webSettingsUserAgent)
+import Data.List (isSuffixOf)
+import Graphics.UI.Gtk.WebKit.DOM.DOMWindow (domWindowGetNavigator)
+import Graphics.UI.Gtk.WebKit.DOM.Navigator (navigatorGetUserAgent)
+import Graphics.UI.Gtk.General.General (priorityHigh)
 
 foreign import ccall safe "ghcjs_currentWindow"
   ghcjs_currentWindow :: IO (Ptr WebView)
@@ -27,25 +40,44 @@ foreign import ccall unsafe "ghcjs_currentDocument"
 currentDocument :: IO (Maybe Document)
 currentDocument = maybeNull (makeNewGObject mkDocument) ghcjs_currentDocument
 
-runWebGUI main = do
+runWebGUI :: (WebView -> IO ()) -> IO ()
+runWebGUI = runWebGUI' "GHCJS"
+
+runWebGUI' :: String -> (WebView -> IO ()) -> IO ()
+runWebGUI' userAgentKey main = do
+  -- Are we in a java script inside some kind of browser
   mbWebView <- currentWindow
   case mbWebView of
-    Just webView -> main webView
+    Just webView -> do
+      -- Check if we are running in javascript inside the the native version
+      Just n <- domWindowGetNavigator (castToDOMWindow webView)
+      agent <- navigatorGetUserAgent n
+      unless ((" " ++ userAgentKey) `isSuffixOf` agent) $ main webView
     Nothing -> do
-      initGUI
-      webView <- makeDefaultWebView
-      result <- main webView
-      mainGUI
-      return result
+      makeDefaultWebView userAgentKey main
 
-makeDefaultWebView = do
+makeDefaultWebView :: String -> (WebView -> IO ()) -> IO ()
+makeDefaultWebView userAgentKey main = do
+  initGUI
   window <- windowNew
+  timeoutAddFull (yield >> return True) priorityHigh 10
   windowSetDefaultSize window 900 600
   windowSetPosition window WinPosCenter
   scrollWin <- scrolledWindowNew Nothing Nothing
   webView <- webViewNew
+  settings <- webViewGetWebSettings webView
+  userAgent <- settings `get` webSettingsUserAgent
+  settings `set` [webSettingsUserAgent := userAgent ++ " " ++ userAgentKey]
+  webViewSetWebSettings webView settings
   window `containerAdd` scrollWin
   scrollWin `containerAdd` webView
   window `onDestroy` mainQuit
   widgetShowAll window
-  return webView
+  webView `on` loadFinished $ \frame -> do
+    main webView
+  args <- getArgs
+  case args of
+    uri:_ -> webViewLoadUri webView uri
+    []    -> main webView
+  mainGUI
+
