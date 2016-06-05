e82c4109f6c81f8b3e423db3031fdcff0e900df6
Maciej Kazulak <kazulakm@gmail.com>
2016-06-05 17:39:08 +0200

    Use two separate FFI imports to avoid the optional argument problem

--- src/GHCJS/DOM/JSFFI/FormData.hs
+++ src/GHCJS/DOM/JSFFI/FormData.hs
@@ -2,47 +2,39 @@
 module GHCJS.DOM.JSFFI.FormData (
     module Generated
   , js_append
+  , js_append3
   , append
   , appendBlob
-  , appendFile
 ) where
 
-import Prelude hiding (appendFile)
 import Control.Monad.IO.Class (MonadIO(..))
 
 import GHCJS.Types (JSVal, JSString)
 import GHCJS.Marshal.Internal (PToJSVal(..))
-import GHCJS.Foreign (jsUndefined)
 import GHCJS.DOM.Types
 
 import GHCJS.DOM.JSFFI.Generated.FormData as Generated hiding (js_append, append)
 
-
-maybeToOptional :: PToJSVal a => Maybe a -> JSVal
-maybeToOptional (Just a) = pToJSVal a
-maybeToOptional Nothing = jsUndefined
-
+foreign import javascript unsafe "$1[\"append\"]($2, $3)"
+        js_append :: FormData -> JSString -> JSVal -> IO ()
 
 foreign import javascript unsafe "$1[\"append\"]($2, $3, $4)"
-        js_append :: FormData -> JSString -> JSVal -> JSVal -> IO ()
+        js_append3 :: FormData -> JSString -> JSVal -> JSString -> IO ()
 
 -- | <https://developer.mozilla.org/en-US/docs/Web/API/FormData/append Mozilla FormData.append documentation>
 append
   :: (MonadIO m, ToJSString name, ToJSString str)
   => FormData -> name -> str -> m ()
 append self name str
-  = liftIO $ js_append self (toJSString name) (pToJSVal str) jsUndefined
+  = liftIO $ js_append self (toJSString name) (pToJSVal str)
 
 -- | <https://developer.mozilla.org/en-US/docs/Web/API/FormData/append Mozilla FormData.append documentation>
 appendBlob
   :: (MonadIO m, ToJSString name, IsBlob blob, ToJSString filename)
   => FormData -> name -> blob -> Maybe filename -> m ()
-appendBlob self name blob filename
-  = liftIO $ js_append self (toJSString name) (unBlob $ toBlob blob) $ maybeToOptional $ toJSString <$> filename
-
--- | <https://developer.mozilla.org/en-US/docs/Web/API/FormData/append Mozilla FormData.append documentation>
-appendFile
-  :: (MonadIO m, ToJSString name, ToJSString filename)
-  => FormData -> name -> File -> Maybe filename -> m ()
-appendFile self name file filename
-  = liftIO $ js_append self (toJSString name) (unFile file) $ maybeToOptional $ toJSString <$> filename
+appendBlob self name blob mfilename
+  = let jsname = toJSString name
+        jsblob = unBlob $ toBlob blob
+    in liftIO $ case mfilename of
+         Just filename -> js_append3 self jsname jsblob $ toJSString filename
+         Nothing -> js_append self jsname jsblob
