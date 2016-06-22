7e7cf8ed117b0e666bcde3781ad74011104acacb
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-06-23 11:29:47 +1200

    Update travis Update Travis build scripts

--- travis/build.sh
+++ travis/build.sh
@@ -1,22 +1,32 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 echo $PATH
 export LC_ALL=C.UTF-8
 
-if [ -d .cabal ]; then cp -a .cabal /root; fi
-if [ -d .ghc ]; then cp -a .ghc /root; fi
+if [[ -d .cabal && -d .ghc ]]; then
+    cp -a .cabal .ghc /root
+fi
 
-cabal --version
-ghc --version
 cabal update
 
-cd ghcjs-dom
-cabal new-build
-cabal new-build -fwebkit
-cd ..
+# Detect if the cache is warmed up.  If so build both versions.
+if ghc-pkg --package-db /root/.cabal/store/ghc-`ghc --numeric-version`/package.db list | grep jsaddle-dom; then
+    cabal new-build
+    cabal new-build -fwebkit
+    all_done=true
+else
+    # Just build jsaddle-dom
+    cabal new-build jsaddle-dom
+    all_done=false
+fi
 
 # update the cache
 rm -rf .cabal
 cp -a /root/.cabal ./
 rm -rf .ghc
 cp -a /root/.ghc ./
+
+if [ "$all_done" = false ]; then
+    echo "Still warming up the Cache.  Please rerun this build."
+    exit 1
+fi
