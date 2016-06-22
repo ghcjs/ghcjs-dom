3dcd2ffa48fccfc28b0ca90e0c8faf7aa134129e
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-06-22 21:21:08 +1200

    Update travis Update Travis build scripts

--- travis/build.sh
+++ travis/build.sh
@@ -9,14 +9,6 @@ if [ -d .ghc ]; then cp -a .ghc /root; fi
 cabal --version
 ghc --version
 cabal update
-cabal install aeson aeson-pretty
-cabal install haskell-gi
-
-# update the cache in case we time out later
-rm -rf .cabal
-cp -a /root/.cabal ./
-rm -rf .ghc
-cp -a /root/.ghc ./
 
 cd ghcjs-dom
 cabal new-build
