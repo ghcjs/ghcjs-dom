5bd611183ed8ff16019f121ccc9e0251147ddbca
John Ericson <git@JohnEricson.me>
2019-12-24 13:57:58 -0500

    Remove Nix, we can just callCabal2nix instead

--- default.nix
+++
@@ -1,5 +0,0 @@
-haskellPackages: {
-  ghcjs-dom = haskellPackages.callPackage ./ghcjs-dom {};
-  ghcjs-dom-jsffi = haskellPackages.callPackage ./ghcjs-dom-jsffi {};
-  ghcjs-dom-jsaddle = haskellPackages.callPackage ./ghcjs-dom-jsaddle {};
-}
--- ghcjs-dom-jsaddle/default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, jsaddle-dom, stdenv }:
-mkDerivation {
-  pname = "ghcjs-dom-jsaddle";
-  version = "0.7.0.3";
-  src = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [ ".git" "dist" ])) ./.;
-  libraryHaskellDepends = [ jsaddle-dom ];
-  description = "DOM library that supports both GHCJS and GHC using jsaddle";
-  license = stdenv.lib.licenses.mit;
-}
