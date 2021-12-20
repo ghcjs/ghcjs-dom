821708ad606b3357563395f8e0b8329caf8ae7c5
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-12-20 17:03:29 +1300

    Add `globalThis` support

--- ghcjs-dom-jsffi/src/GHCJS/DOM.hs
+++ ghcjs-dom-jsffi/src/GHCJS/DOM.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE CPP, ForeignFunctionInterface, OverloadedStrings #-}
 {-# LANGUAGE JavaScriptFFI #-}
 module GHCJS.DOM (
-  currentWindow
+  globalThis
+, globalThisUnchecked
+, currentWindow
 , currentWindowUnchecked
 , currentDocument
 , currentDocumentUnchecked
@@ -26,11 +28,48 @@ import JavaScript.Web.AnimationFrame (waitForAnimationFrame, AnimationFrameHandl
 
 import GHCJS.DOM.Types
 
+-- This type is used to access the `globalThis` (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis)
+newtype GlobalThis = GlobalThis { unGlobalThis :: JSVal }
+
+instance Eq (GlobalThis) where
+  (GlobalThis a) == (GlobalThis b) = js_eq a b
+
+instance PToJSVal GlobalThis where
+  pToJSVal = unGlobalThis
+  {-# INLINE pToJSVal #-}
+
+instance PFromJSVal GlobalThis where
+  pFromJSVal = GlobalThis
+  {-# INLINE pFromJSVal #-}
+
+instance ToJSVal GlobalThis where
+  toJSVal = return . unGlobalThis
+  {-# INLINE toJSVal #-}
+
+instance FromJSVal GlobalThis where
+  fromJSVal = return . fmap GlobalThis . maybeJSNullOrUndefined
+  {-# INLINE fromJSVal #-}
+
+instance IsEventTarget GlobalThis
+instance IsWindowOrWorkerGlobalScope GlobalThis
+instance IsGlobalPerformance GlobalThis
+instance IsGlobalEventHandlers GlobalThis
+instance IsGlobalCrypto GlobalThis
+noGlobalThis :: Maybe GlobalThis
+noGlobalThis = Nothing
+{-# INLINE noGlobalThis #-}
+
+foreign import javascript unsafe "$r = globalThis"
+  ghcjs_globalThis :: IO (Nullable GlobalThis)
 foreign import javascript unsafe "$r = window"
   ghcjs_currentWindow :: IO (Nullable Window)
 foreign import javascript unsafe "$r = document"
   ghcjs_currentDocument :: IO (Nullable Document)
 
+globalThis :: MonadDOM m => m (Maybe GlobalThis)
+globalThis = liftDOM $ nullableToMaybe <$> ghcjs_currentGlobalThis
+globalThisUnchecked :: MonadDOM m => m GlobalThis
+globalThisUnchecked = liftDOM $ fromJust <$> currentGlobalThis
 currentWindow :: MonadDOM m => m (Maybe Window)
 currentWindow = liftDOM $ nullableToMaybe <$> ghcjs_currentWindow
 currentWindowUnchecked :: MonadDOM m => m Window
